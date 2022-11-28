f9daa44b3a1c5e4432947c03e28eee13fdbfe70b
plocandido <plo.candido@outlook.com>
2022-11-28 16:44:24 -0300

    update _site

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221127.164101';
+const cacheName = 'chirpy-20221128.164401';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
